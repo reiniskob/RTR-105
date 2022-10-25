c6697a57cdfb2a120efae1c8954584b93c442a48
reiniskob <reiniskob@gmail.com>
2022-10-25 18:33:54 +0300

    RTR-105

---
+++ Uzdevumi/for_factorial.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+int main()
+{
+        int n, test;
+        char t;
+        printf("Enter a whole decimal number:");
+        scanf(" %d",&n);    //pajauta faktoriala decimālo skaitli
+        printf("Choose a data type: \nFor char enter \"c\" \nFor int enter \"i\" \nFor long long int enter \"l\"\nc,i,l:");
+        scanf(" %c",&t);    //pajauta faktoriala decimālo skaitli
+        if(t == 99)
+        {
+                printf("Char n!: ");
+                char fact = 1, fact_previous = 0, fact_next;
+                int x = 0;
+                        for(;x <= n;)
+                        {
+                                if(fact_previous == fact_next ) //parbauda ieprieksejo ar nakamo dalits ar x
+                                {
+                                        fact_previous = fact;
+                                        x = x + 1;
+                                        fact = fact * x;
+                                        fact_next = fact / x;
+                                }
+                                else
+                                {
+                                        x = n+2;                // partrauc loop ja fact_previous nav vienads ar fact_next
+                                        printf("can't dispaly choosen factorial\n");
+                                }
+                        }
+                if(x != n+2)            //ja faktorials ir iespejams parada ieprieksejo iespejamo faktorialu
+                {
+                        printf("%d\n", fact_previous);
+                }
+        }
+        else
+        {
+                if(t == 105)
+        	{
+                printf("Char n!: ");
+                int fact = 1, fact_previous = 0, fact_next;
+                int x = 0;
+                        for(;x <= n;)
+                        {
+                                if(fact_previous == fact_next ) //parbauda ieprieksejo ar nakamo dalits ar x
+                                {
+                                        fact_previous = fact;
+                                        x = x + 1;
+                                        fact = fact * x;
+                                        fact_next = fact / x;
+                                }
+                                else
+                                {
+                                        x = n+2;                // partrauc loop ja fact_previous nav vienads ar fact_next
+                                        printf("can't dispaly choosen factorial\n");
+                                }
+                        }
+                if(x != n+2)            //ja faktorials ir iespejams parada ieprieksejo iespejamo faktorialu
+                {
+                        printf("%d\n", fact_previous);
+                }
+        	}
+        	else
+        	{
+        	        if(t == 108)
+       			{
+				printf("Char n!: ");
+				long long int fact = 1, fact_previous = 0, fact_next;
+				int x = 0;
+				        for(;x <= n;)
+				        {
+				                if(fact_previous == fact_next ) //parbauda ieprieksejo ar nakamo dalits ar x
+				                {
+				                        fact_previous = fact;
+				                        x = x + 1;
+				                        fact = fact * x;
+				                        fact_next = fact / x;
+				                }
+				                else
+				                {
+				                        x = n+2;                // partrauc loop ja fact_previous nav vienads ar fact_next
+				                        printf("can't dispaly choosen factorial\n");
+				                }
+				        }
+				if(x != n+2)            //ja faktorials ir iespejams parada ieprieksejo iespejamo faktorialu
+				{
+				        printf("%lld\n", fact_previous);
+				}
+			}
+			else
+			{
+			printf("Bad data type");
+			}
+        	}
+        }
+
+   return 0;
+}
--- Uzdevumi/while_factorial.c
+++ Uzdevumi/while_factorial.c
@@ -5,28 +5,36 @@ int main()
 	char t;
 	printf("Enter a whole decimal number:");
 	scanf(" %d",&n);    //pajauta faktoriala decimālo skaitli
-	printf("Choose a data type: \nFor char enter \"c\" \nFor int enter\"i\" \nFor long long int enter \"l\"\nc,i,l:");
+	printf("Choose a data type: \nFor char enter \"c\" \nFor int enter \"i\" \nFor long long int enter \"l\"\nc,i,l:");
 	scanf(" %c",&t);    //pajauta faktoriala decimālo skaitli
 	if(t == 99)
 	{
-		printf("Char:\n");
-		char fact = 1;
+		printf("Char n!: ");
+		char fact = 1, fact_previous = 0, fact_next;
 		int x = 0;
-//		long long int fact_check;
-//		while(fact == fact_check)
-//		{
-			for(;x < n;)
+			for(;x <= n;)
 			{
-				x = x + 1;
-				fact = fact * x;
-				printf("%d!=%d\n", x, fact);
+				if(fact_previous == fact_next ) //parbauda ieprieksejo ar nakamo dalits ar x
+				{
+					fact_previous = fact;
+					x = x + 1;
+					fact = fact * x;
+					fact_next = fact / x;
+				}
+				else
+				{
+					x = n+2;		// partrauc loop ja fact_previous nav vienads ar fact_next
+					printf("can't dispaly choosen factorial\n");
+				}
 			}
-//		}
+		if(x != n+2)		//ja faktorials ir iespejams parada ieprieksejo iespejamo faktorialu
+		{
+			printf("%d\n", fact_previous);
+		}
 	}
 	else
 	{
-		printf("Bad data type");
-	}
+
    return 0;
 }
 
