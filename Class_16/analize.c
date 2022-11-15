f5ac4a9f82dd6f28337ef0c33df0ac14fe890802
reiniskob <reiniskob@gmail.com>
2022-11-15 09:51:07 +0200

    RTR-105

---
+++ Class_16/analize.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+        int i, N, n;
+        char teikums [N];
+        printf("Enter a sentence:");
+	gets(teikums);
+	printf("You entered: %s.", teikums);
+
+//	for(i=97;i>91;i++)
+//	{
+//		for(n=0;n<N;n++)
+//		{
+//			printf("%c\n", teikums[n]);
+//		}
+//		printf("\n");
+//	}
+
+//	for(n=0;teikums[n]>=97;n++)
+//		for(i=97;i==122;i++)
+//		{
+//			if(teikums[n]==i)
+//			{
+//
+//			}
+//		}
+
+return 0;
+}
+
