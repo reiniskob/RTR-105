12bd23c746b89f960e73d88e6af546e567336c23
reiniskob <reiniskob@gmail.com>
2022-11-15 12:53:47 +0200

    RTR-105

--- Class_16/analize.c
+++ Class_16/analize.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
 int main()
 {
-        int i, N, n;
-        char teikums [N];
+        int i, N, n, x;
+        char teikums[N];
         printf("Enter a sentence:");
-	gets(teikums);
-	printf("You entered: %s.", teikums);
+	fgets(teikums, N, stdin);
+//	gets(teikums);
+	printf("You entered: %s", teikums);
 
-//	for(i=97;i>91;i++)
-//	{
-//		for(n=0;n<N;n++)
-//		{
-//			printf("%c\n", teikums[n]);
-//		}
-//		printf("\n");
-//	}
-
-//	for(n=0;teikums[n]>=97;n++)
-//		for(i=97;i==122;i++)
-//		{
-//			if(teikums[n]==i)
-//			{
-//
-//			}
-//		}
+	for(i=97;i<123;i++)//iet caur alfabetam
+	{
+		x = 0;
+		for(n=0;n<N;n++)// iet cauri tekstam
+		{
+			if(teikums[n]==i)
+			{
+				x++;
+			}
+		}
+		char c = i;
+		printf("\n%c repetes %d",c , x );
+	}
+	printf("\n");
 
 return 0;
 }
